234caf0fb919dcabb855bb32257696589f9f5f60
Fionnuala Carr <fcarr@kx.com>
2018-09-24 14:43:34 +0100

    gets raw test file from embedPy

--- build/test.bat
+++ build/test.bat
@@ -1,5 +1,5 @@
 if defined QLIC_KC (
         pip -q install -r requirements.txt
-        curl -fsSL -o test.q https://github.com/KxSystems/embedPy/blob/master/test.q
+        curl -fsSL -o test.q https://github.com/KxSystems/embedpy/raw/master/test.q
         q test.q utils\tests fresh\tests
 )
